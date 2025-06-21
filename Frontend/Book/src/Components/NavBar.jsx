01e9399fec95661b10c7f91e6efa3f73b2add534
VaishnavAmbilpur <vaishnavambilpur2006@gmail.com>
2025-06-22 04:10:56 +0530

    fix

--- Frontend/Book/src/Components/NavBar.jsx
+++ Frontend/Book/src/Components/NavBar.jsx
@@ -7,12 +7,12 @@ import { Link } from 'react-router'
      <header className='bg-base-100 border-b border-base-content/15'>
         <div className='mx-auto max-w-6xl p-4'>
           <div className='flex items-center justify-between font-extrabold '>
-            <h1 className='font-extrabold tracking-tighter text-6xl text-amber-100'>
+            <h1 className='font-extrabold tracking-tighter text-3xl text-amber-100'>
                 THINKBOLD
             </h1>
             <div className='flex items-center gap-4 font-extrabold text-zinc-950'>
              <Link to={'/create'} className='btn btn-accent'>
-             <PlusIcon className="size-7  font-extrabold text-zinc-950"></PlusIcon>
+             <PlusIcon className="size-5  font-extrabold text-zinc-950"></PlusIcon>
              <span className='font-extrabold text-zinc-950'>New Note</span>
              </Link>
              
