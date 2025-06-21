46f6128ca1c669891b2b2c8f1cc83ccdd4d59c1a
VaishnavAmbilpur <vaishnavambilpur2006@gmail.com>
2025-06-22 02:47:36 +0530

    fix

--- Backend/db.js
+++ Backend/db.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 const connnect = async () =>{
     try{
-        await mongoose.connect(process.env.MONGO)
+        await mongoose.connect("mongodb+srv://vaishnavambilpur2006:Vaishnav%4011@cluster0.nhu16gt.mongodb.net/Notes")
         console.log("Connected");   
     } catch(err){
       console.log("Error Connecting the mongodb")
